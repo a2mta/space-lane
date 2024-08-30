215b14fd2bff2a7fd1b8045d1c48e1fc2660fb84
Dmitrii Gusarov <dmitry_g@mail.com>
2024-08-30 16:31:32 +0300

    consulting page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -6,24 +6,71 @@ const projects = [
   {
     title: 'Рублево — Архангельское',
     subTitle: 'Благоустройство, авторский надзор',
+    link: 'rublevo',
+    image: '/pics/projects/rublevo.png',
   },
   {
     title: 'Коттеджный поселок «Новосельцево»',
     subTitle: 'Исследование, благоустройство',
+    link: 'novoseltsevo',
+    image: '/pics/projects/novoseltsevo.png',
   },
   {
     title: 'Махтумкули Фраги',
     subTitle: 'Исследование, благоустройство',
+    link: 'fragi',
+    image: '/pics/projects/fragi.png',
+  },
+  {
+    title: 'Коттеджный посёлок «Ели»',
+    subTitle: 'Исследование, мастер-план, архитектура, благоустройство',
+    link: 'ely',
+    image: '/pics/projects/ely.png',
+  },
+  {
+    title: 'Коттеджный поселок «Максимово парк»',
+    subTitle: 'Исследование, мастер-план, дизайн-код, благоустройство',
+    link: 'maksimovo',
+    image: '/pics/projects/maksimovo.png',
+  },
+  {
+    title: 'ЖК Corso',
+    subTitle: 'Благоустройство',
+    link: 'corso',
+    image: '/pics/projects/corso.png',
+  },
+  {
+    title: 'Дарьино парк',
+    subTitle: 'Благоустройство',
+    link: 'darino',
+    image: '/pics/projects/darino.png',
+  },
+  {
+    title: 'Олений парк',
+    subTitle: 'Исследование, мастер-план, архитектура, благоустройство',
+    link: 'oleniy-park',
+    image: '/pics/projects/oleniy-park.png',
+  },
+  {
+    title: 'Савеловский кластер',
+    subTitle: 'Исследование, благоустройство',
+    link: 'savelovskiy',
+    image: '/pics/projects/savelovskiy.png',
   },
 ];
 
 const ProjectsPage = () => {
   return (
     <PageWithTitleWrap title='Проекты'>
-      <div className='grid grid-cols-12 gap-x-10 mt-30'>
+      <div className='grid grid-cols-12 gap-x-10 mt-30 gap-y-10'>
         {projects.map((item, index) => (
-          <div key={index+item.title} className='col-span-4 relative '>
-            <ProjectCard title={item.title} subTitle={item.subTitle} url='' />
+          <div key={index + item.title} className='col-span-4 relative '>
+            <ProjectCard
+              title={item.title}
+              subTitle={item.subTitle}
+              link={item.link}
+              image={item.image}
+            />
           </div>
         ))}
       </div>
--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -2,18 +2,19 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React, { FC } from 'react';
 
-const ProjectCard: FC<{ url: string; title: string; subTitle: string }> = ({
+const ProjectCard: FC<{image:string, link: string; title: string; subTitle: string }> = ({
   subTitle,
   title,
-  url,
+  link,
+  image
 }) => {
   return (
     <div className='group cursor-pointer'>
-      <Link href="project/1" style={{display: 'flex', flexDirection: 'column'}}>
+      <Link href={`project/${link}`} style={{display: 'flex', flexDirection: 'column'}}>
         <div className='overflow-hidden'>
           <Image
             className='group-hover:scale-110 transition-transform duration-700 ease-out aspect-w-10 aspect-h-9'
-            src='/pics/projects/rublevo.png'
+            src={image}
             height={560}
             width={560}
             alt='project'
