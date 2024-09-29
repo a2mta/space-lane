d362124d32f0553a5d83c1c13ddcc36a6712f5dc
Dmitrii Gusarov <dmitry_g@mail.com>
2024-09-29 22:21:30 +0300

    fixes

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -2,19 +2,14 @@ import classNames from 'classnames';
 import React, { FC, PropsWithChildren } from 'react';
 
 const Grid: FC<
-  PropsWithChildren & {
-    className?: string;
-    cols?: number;
-    colsM?: number;
-    gapX?: number;
-  }
-> = ({ className, children, cols, colsM, gapX }) => {
+  PropsWithChildren & { className?: string; cols?: number; colsM?: number, gapX?: number }
+> = ({ className, children, cols, colsM,gapX }) => {
   return (
     <div
       className={classNames(
         'grid',
-        `grid-cols-${colsM || 2}`,
         `lg:grid-cols-${cols || 12}`,
+        `grid-cols-${colsM || 2}`,
         `gap-x-${gapX || 10}`,
         className
       )}
