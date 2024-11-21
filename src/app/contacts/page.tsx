8ab5dbb059002c18fa8b40cde7b3efadcb960583
Dmitrii Gusarov <dmitry_g@mail.com>
2024-11-21 14:23:03 +0300

    contacts

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -7,23 +7,28 @@ const ContactsPage = () => {
     <PageMarginWithTitle withBorder title='Контакты'>
       <Grid className='pt-6'>
         <div className='hidden md:flex col-start-5 col-span-1'>
-          <span className='font-medium text-h5 text-grey'>Соц.сети</span>
+          <span className='font-medium md:text-h6 xl:text-h5 text-grey mr-2'>Соц.сети</span>
         </div>
-        <div className='hidden md:flex text-h5-mob md:text-h5 font-medium col-span-3 space-y-2  flex-col'>
+        <div className='hidden md:flex text-h5-mob md:text-h6 xl:text-h5 font-medium col-span-3 space-y-2  flex-col'>
           <span>Inst*gram</span>
           <span>Behance</span>
           <span>Linkedin</span>
         </div>
-        <div className='mb-20 md:mb-0 col-span-2 md:col-span-4 mt-30'>
-          <span className='text-h5-mob md:text-h5 font-medium text-grey'>
+        <div className='mb-20 md:mb-0 col-span-2 md:col-span-4 mt-30 md:mt-0'>
+          <span className='text-h5-mob md:text-h6 xl:text-h5 font-medium text-grey'>
             По вопросам предоставления услуг, организации сотрудничества и
             партнёрств
           </span>
-          <div className='text-body-regular-mob md:text-h6 xl:text-h5 font-light flex flex-col space-y-4 md:space-y-1 mt-10 md:mt-6'>
+          <div className='text-body-regular-mob md:text-h6 xl:text-h5 font-light flex flex-col space-y-4 md:space-y-2 mt-10 md:mt-6'>
             <span className='block'>Info@spacelane.ru</span>
             <span className='block'>spacelanemsk@gmail.ru</span>
             <span className='block'>+7 926 066-36-69</span>
           </div>
+          <button className='mt-10 max-w-56 p-2 bg-transparent hover:bg-transparent text-inherit font-inherit m-0 cursor-pointer border border-grey flex items-center justify-center'>
+            <span className='text-body-regular font-medium'>
+              Заполнить бриф
+            </span>
+          </button>
           <Grid className='mt-4 md:hidden'>
             <div className='col-span-1'>
               <button className='p-2 bg-transparent hover:bg-transparent text-inherit font-inherit m-0 cursor-pointer border border-grey'>
@@ -32,12 +37,12 @@ const ContactsPage = () => {
             </div>
           </Grid>
         </div>
-        <div className='col-span-1'>
+        <div className='col-span-1 md:hidden'>
           <span className='text-h5-mob font-medium text-medium-grey'>
             Соц.сети
           </span>
         </div>
-        <div className='col-span-1'>
+        <div className='col-span-1 md:hidden'>
           <div className='flex flex-col space-y-2'>
             <span>Inst*gram</span>
             <span>Behance</span>
