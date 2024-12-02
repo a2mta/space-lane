c54551d830c2b3d6fa28fc12cf148e3a4d2140e6
Dmitrii Gusarov <dmitry_g@mail.com>
2024-12-02 22:53:38 +0300

    desktop tooltip

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,22 +1,36 @@
 import React from 'react';
 import Grid from '../Grid';
 import PageMarginWithTitle from '../PageMarginWithTitle';
+import Tooltip from '../Tooltip'; // Import Tooltip
 
 const Footer = () => {
   return (
     <footer className='font-manrope '>
       <PageMarginWithTitle className='mt-70 mb-14 hidden md:block'>
         <Grid className='md:grid '>
-          <span className='col-span-4'>Rus / Eng</span>
+          <span className='col-span-4'>
+            <div>
+              <span className='relative'>
+                Rus /{' '}
+                <Tooltip>
+                  <span className='text-medium-grey cursor-pointer'>Eng</span>
+                </Tooltip>
+              </span>
+            </div>
+          </span>
           <span className='col-span-7'>Instagram / Telegram</span>
-          <span className='col-span-1 text-right'>© {new Date().getFullYear()}</span>
+          <span className='col-span-1 text-right'>
+            © {new Date().getFullYear()}
+          </span>
         </Grid>
       </PageMarginWithTitle>
       <PageMarginWithTitle className='md:hidden my-10'>
         <Grid>
           <div className='col-span-2 text-body-regular-mob font-medium'>
             <div className='flex justify-between'>
-              <span>Rus / Eng</span>
+              <Tooltip>
+                <span>Rus / Eng</span>
+              </Tooltip>
               <span>Instagram / Telegram</span>
               <span>© {new Date().getFullYear()}</span>
             </div>
---
+++ src/components/Tooltip.tsx
@@ -0,0 +1,31 @@
+'use client';
+import Image from 'next/image';
+import React, { FC, ReactElement, useState } from 'react';
+
+const Tooltip: FC<{ children: ReactElement }> = ({ children }) => {
+  const [isTooltipVisible, setTooltipVisibility] = useState(false);
+  const toggleTooltip = () => setTooltipVisibility(!isTooltipVisible);
+  return (
+    <>
+      {React.cloneElement(children, { onClick: toggleTooltip })}
+      {isTooltipVisible && (
+        <div className='absolute  py-2 bg-white  w-[210px] h-[60px] left-[65px] top-[-8px]'>
+          <Image alt='' src='/icons/tooltip-bg.svg' fill />
+          <span className='pl-6 pr-9 text-body-caption font-medium block top-4 absolute'>
+            The English version of the site is currently under development
+          </span>
+          <Image
+            onClick={toggleTooltip}
+            src='/icons/tooltip-cross.svg'
+            width={11}
+            height={12}
+            alt=''
+            className='absolute top-[10px] right-[7px] cursor-pointer'
+          />
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Tooltip;
