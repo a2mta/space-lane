5584c870d5148b19bae9e996de52dc5f6491fc54
Dmitrii Gusarov <dmitry_g@mail.com>
2026-05-06 00:07:06 +0300

    turned off about page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div className="flex min-h-[55vh] w-full items-center justify-center">
+      <span>Страница находится в разработке</span>
+    </div>
+  );
+}
